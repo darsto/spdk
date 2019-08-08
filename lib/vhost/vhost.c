3417659b8064963b448898b0846726aad343041b
Darek Stojaczyk <dariusz.stojaczyk@intel.com>
2019-08-08 14:08:03 +0200

    rte_vhost: unaffinitize the thread inside rte_vhost API function
    We used to call rte_vhost_driver_start() under
    spdk_call_unaffinitized() because that function could
    spawn a new pthread and we didn't want to to be pinned
    to the one single cpu of the current SPDK reactor.
    
    New DPDK versions (>= 19.05) already unaffinitize the
    pthread by themselves, so our spdk_call_unaffinitized()
    was only required for the legacy, internal rte_vhost fork
    in SPDK. To clean up SPDK code, move the un-affinitization
    down to the rte_vhost fork.
    
    Change-Id: I53836517e9ec2ff366b509f00e1403845e3c3172
    Signed-off-by: Darek Stojaczyk <dariusz.stojaczyk@intel.com>

--- lib/vhost/vhost.c
+++ lib/vhost/vhost.c
@@ -581,18 +581,6 @@ vhost_parse_core_mask(const char *mask, struct spdk_cpuset *cpumask)
 	return 0;
 }
 
-static void *
-_start_rte_driver(void *arg)
-{
-	char *path = arg;
-
-	if (rte_vhost_driver_start(path) != 0) {
-		return NULL;
-	}
-
-	return path;
-}
-
 extern const struct vhost_device_ops g_dpdk_vhost_ops;
 
 int
@@ -696,12 +684,7 @@ vhost_dev_register(struct spdk_vhost_dev *vdev, const char *name, const char *ma
 
 	vhost_dev_install_rte_compat_hooks(path);
 
-	/* The following might start a POSIX thread that polls for incoming
-	 * socket connections and calls backend->start/stop_device. These backend
-	 * callbacks are also protected by the global SPDK vhost mutex, so we're
-	 * safe with not initializing the vdev just yet.
-	 */
-	if (spdk_call_unaffinitized(_start_rte_driver, path) == NULL) {
+	if (rte_vhost_driver_start(path) != 0) {
 		SPDK_ERRLOG("Failed to start vhost driver for controller %s (%d): %s\n",
 			    name, errno, spdk_strerror(errno));
 		rte_vhost_driver_unregister(path);
